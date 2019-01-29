dffc31bb571c89df01596103668ebf4d821f2f4e
Justin <justinnegus@gmail.com>
2019-01-28 21:42:03 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,76 @@
+const Discord = require('discord.js');
+const bot  = new Discord.Client();
+bot.login('NTM5Mjg4NTU0NDUxMjM4OTIz.DzAMXg.l8mZFH8hJh_bI2nafWaCd9lUYaE');
+
+
+bot.on('ready', () => {
+    console.log('I am running');
+});
+
+bot.on('message', message => {
+    if (!message.guild) return;
+    if (message.content.startsWith('!kick')) {
+        const user = message.mentions.users.first();
+        if (user) {
+            const member = message.guild.member(user);
+            if (member) {
+                member.kick().then(() => {
+                    message.reply(`${member}, has been removed`);
+                }).catch(err => {
+                    message.reply('Unable to kick the member!');
+                    console.error(err);
+                });
+            } else {
+                message.reply('That user isn\'t in this guild ');
+            }
+        } else {
+            message.reply('Please mention a correct user  to kick!');
+        }
+    }
+});
+
+
+bot.on('message', msg => {
+    let prefix = '!',
+    addrole = 'addrole',
+    removerole = 'removerole';
+
+    if(msg.content.toLowerCase().startsWith(prefix + addrole)) {
+        let args = msg.content.split(" ");
+        if(args.length < 2 || args.length > 2) {
+            msg.channel.send("Please request an available role");
+        }
+        if(args[1] === 'adc' || args[1] === 'mid' || args[1] === 'support' || args[1] === 'jungle' || args[1] === 'top') {
+            let role = msg.guild.roles.find(role => role.name === args[1].toUpperCase());
+            msg.member.addRole(role)
+            msg.channel.send(`${role} role has been succesfully added`)
+        }
+    } else if(msg.content.toLowerCase().startsWith(prefix + removerole)) {
+        let args = msg.content.split(" ");
+        if(args.length < 2 || args.length > 2) {
+            msg.channel.send("Please request an available role");
+        }
+        if(args[1] === 'adc' || args[1] === 'mid' || args[1] === 'support' || args[1] === 'jungle' || args[1] === 'top') {
+            let role = msg.guild.roles.find(role => role.name === args[1].toUpperCase());
+            msg.member.removeRole(role)
+            msg.channel.send(`${role} role has been succesfully removed`);
+        }
+    }
+});
+
+
+
+
+bot.on('guildMemberAdd', member => {
+    const memberRole = member.guild.roles.find(role => role.name === 'Summoner');
+    const channel = member.guild.channels.find(ch => ch.name === 'welcome');
+    if(!channel) return;
+        channel.send(`Welcome to the server, ${member}`);
+    member.addRole(memberRole);
+});
+
+
+
+
+    
+
---
+++ help.js
@@ -0,0 +1,34 @@
+
+
+const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern State College","email":"ctorry0@macromedia.com"},
+{"id":2,"first_name":"Saundra","university":"The School of the Art Institute of Chicago","email":"swhal1@state.gov"},
+{"id":3,"first_name":"Lambert","university":"Marian College","email":"lparham2@techcrunch.com"},
+{"id":4,"first_name":"Modestine","university":"International Medical & Technological University","email":"mdolder3@symantec.com"},
+{"id":5,"first_name":"Chick","university":"Sultan Salahuddin Abdul Aziz Shah Polytechnic","email":"camorts4@google.com.au"},
+{"id":6,"first_name":"Jakob","university":"Fachhochschule Rosenheim, Hochschule für Technik und Wirtschaft","email":"jharken5@spiegel.de"},
+{"id":7,"first_name":"Robbi","university":"Salem University","email":"rbrister6@redcross.org"},
+{"id":8,"first_name":"Colline","university":"Coastal Carolina University","email":"cbrosh7@alibaba.com"},
+{"id":9,"first_name":"Michail","university":"Universidad Católica de Ávila","email":"mrome8@shinystat.com"},
+{"id":10,"first_name":"Hube","university":"Universitat Rovira I Virgili Tarragona","email":"hlethbrig9@foxnews.com"}
+]
+
+
+
+
+
+
+
+const uni = [];
+let substring = 'Uni';
+for(let i = 0; i < graduates.length; i++) {
+  if(graduates[i].university.includes(substring)) {
+   uni.push(graduates[i]);
+  }
+}
+console.log(uni);
+
+
+
+
+
+
